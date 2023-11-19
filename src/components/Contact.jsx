33603c2b3ee8e516abc7d22fbd46b9abfecf67c5
muslumakgul <muslum280@gmail.com>
2023-11-19 13:15:54 +0300

    Second commit

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -22,7 +22,7 @@ const Contact = () => {
   const handleSubmit = (e) => {};
 
   return (
-    <div className="xl:mt-12 xl:flex-row flex-column-reverse flex gap-10 overflow-hidden">
+    <div className={`xl:mt-12 flex xl:flex-row flex-col-reverse gap-10 overflow-hidden`}>
       <motion.div
         variants={slideIn("left", "tween", 0.2, 1)}
         className="flex-[0.75] bg-black-100 p-8 rounded-2xl"
@@ -70,7 +70,7 @@ const Contact = () => {
 
           <button
             type="submit"
-            className="bg-tertiary py-3 px-8 outline-none w-fit text-white font-bold shadow-md shadow-primary rounded-xl"
+            className="bg-tertiary py-3 px-8 rounded-xl outline-none w-fit text-white font-bold shadow-md shadow-primary "
           >
             {loading ? "Sending..." : "Send"}
           </button>
